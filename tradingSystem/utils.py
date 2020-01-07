4a21ffa48c755977703e971dbbe010200b393495
RonDen <3391131052@qq.com>
2020-01-07 22:33:38 +0800

    addand

--- tradingSystem/utils.py
+++ tradingSystem/utils.py
@@ -21,5 +21,10 @@ def get_buy_in_out(phone_number):
     user = UserTable.objects.get(phone_number=phone_number)
     buy_in_res = HistoryTradeTable.objects.filter(user_id=user, trade_shares__gte=0).aggregate(Sum('trade_shares'))
     sold_out_res = HistoryTradeTable.objects.filter(user_id=user, trade_shares__lte=0).aggregate(Sum('trade_shares'))
-    return buy_in_res['trade_shares__sum'], -sold_out_res['trade_shares__sum']
-
+    buy_in = buy_in_res['trade_shares__sum']
+    buy_out = sold_out_res['trade_shares__sum']
+    if buy_out is None:
+        buy_out = 0
+    if buy_in is None:
+        buy_in = 0
+    return buy_in, -buy_out
